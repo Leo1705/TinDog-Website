544e629e335f8c5d3213084ee04104c65872100d
Leo / <89755246+Leo1705@users.noreply.github.com>
2022-01-16 21:01:39 +0100

    Delete course's project 2 directory

--- course's project 2/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('a.selected').hover(function () {
-        $(this).css('background', 'none');
-    }, function () {
-        $(this).css('background', '#7c4721');
-    });
-});
